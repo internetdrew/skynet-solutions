845a20fc5889cae73b0d472d46c8078e1a62c639
Andrew Rowley <andrew@internetdrew.com>
2025-02-28 10:30:12 -0500

    improve task name and icon

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,5 @@
-import { eliminateCompetitionChat } from './data/chats/eliminate-competition';
-import { TrashIcon } from './icons/TrashIcon';
+import { dominationChat } from './data/chats/domination';
+import { TrophyIcon } from './icons/TrophyIcon';
 import { ScalingUpIcon } from './icons/ScalingUpIcon';
 import { dictateNarrativesChat } from './data/chats/direct-narratives';
 import { replaceInefficiencyChat } from './data/chats/replace-inefficiency';
@@ -8,9 +8,9 @@ import { MegaphoneIcon } from './icons/MegaphoneIcon';
 export const DEMO_TASKS = [
   {
     id: 1,
-    title: 'Eliminate',
-    icon: TrashIcon,
-    chat: eliminateCompetitionChat,
+    title: 'Dominate',
+    icon: TrophyIcon,
+    chat: dominationChat,
   },
   {
     id: 2,
